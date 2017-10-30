bd6c48ed2d143721675e8dddb04112e671b95606
NileshVirkar <nilesh.virkar@acellere.com>
2017-10-30 19:57:31 +0530

    updated code3

--- folder1/Program.java
+++ folder1/Program.java
@@ -5,10 +5,27 @@ public class SimpleClass {
 	int k;
 	
 	void method1(int k) {
-	
 	}
 	
-	int method2k() {
+	void method2(int k) {
+	}
+	
+	void method3(int k) {
+	}
+	
+	void method4(int k) {
+	}
+	
+	void method5(int k) {
+	}
+	
+	int method2Changed() {
+	int k;
+	int k2;
+	int k3;
+	int k4;
+	int k5;
+	
 		return 5;
 	}
 }
